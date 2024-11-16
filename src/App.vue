a14dfc7a4f3d7ec9e91e7d69bd353dde0cac1a92
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-16 23:33:16 +0700

    Setting Router

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,19 @@
       </router-link>
       <div class="flex items-center gap-2">
         <router-link
-          to="/"
+          :to="{ name: 'byName' }"
           class="inline-flex items-center h-full px-2 transition-colors hover:bg-teal-100"
         >
           Search Meals
         </router-link>
         <router-link
-          to="/"
+          :to="{ name: 'byLetter' }"
           class="inline-flex items-center h-full px-2 transition-colors hover:bg-teal-100"
         >
           Meals By Letter
         </router-link>
         <router-link
-          to="/"
+          :to="{ name: 'byIngredient' }"
           class="inline-flex items-center h-full px-2 transition-colors hover:bg-teal-100"
         >
           Meals By Ingredients
