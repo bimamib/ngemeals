e754a1c1a23ce17e40605717ca976324c50cece6
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-16 23:09:59 +0700

    Add router link

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,39 @@
 <template>
-  <router-view />
+  <div class="h-full bg-gray-100">
+    <header
+      class="flex items-stretch justify-between h-16 bg-white border shadow"
+    >
+      <router-link
+        :to="{ name: 'home' }"
+        class="inline-flex items-center h-full px-5"
+      >
+        Home
+      </router-link>
+      <div class="flex items-center gap-2">
+        <router-link
+          to="/"
+          class="inline-flex items-center h-full px-2 transition-colors hover:bg-teal-100"
+        >
+          Search Meals
+        </router-link>
+        <router-link
+          to="/"
+          class="inline-flex items-center h-full px-2 transition-colors hover:bg-teal-100"
+        >
+          Meals By Letter
+        </router-link>
+        <router-link
+          to="/"
+          class="inline-flex items-center h-full px-2 transition-colors hover:bg-teal-100"
+        >
+          Meals By Ingredients
+        </router-link>
+      </div>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <script setup></script>
