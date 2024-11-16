4e0d840a2dede7f0d00faff2825eca2c3de399df
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-16 20:24:39 +0700

    Settings Route Letter

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
     name: "home",
     component: Home,
   },
+  {
+    path: "/letter/:letter",
+    name: "byLetter",
+    component: Home,
+  },
 ];
 
 const router = createRouter({
