e4a75dbfb4d59b7a031d5f89f20ae6551c1cf888
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-16 20:23:49 +0700

    Add Search bar input

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,48 @@
-<template>Home</template>
+<template>
+  <div class="flex flex-col p-8">
+    <div class="relative">
+      <div
+        class="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-3.5"
+      >
+        <svg
+          class="text-gray-400 shrink-0 size-4"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle cx="11" cy="11" r="8"></circle>
+          <path d="m21 21-4.3-4.3"></path>
+        </svg>
+      </div>
+      <input
+        class="block w-full text-sm border-gray-200 rounded-lg ps-10 pe-4 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+        type="text"
+        placeholder="Search for Meals..."
+      />
+    </div>
+    <!-- <input
+      type="text"
+      class="w-full border-gray-300 rounded-lg"
+      placeholder="Search for Meals..."
+    /> -->
 
-<script setup></script>
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link to="/" v-for="letter of letters">
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+</script>
