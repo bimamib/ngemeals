b24a520e29ff3d8fa00e5883a12a1e5f580425fb
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-16 23:57:27 +0700

    Add: Default Layout (Home)

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="h-full bg-gray-100">
+    <navbar />
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Navbar from "../components/Navbar.vue";
+</script>
