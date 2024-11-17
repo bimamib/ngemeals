760fb4c6fb7d0bd305a22f1bfd5672d8563bdeee
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-17 21:31:48 +0700

    Edit Melas By Name

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -29,17 +29,15 @@
       />
     </div>
   </div>
-  <div class="grid grid-cols-1 gap-5 p-8 md:grid-cols-3">
-    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
-  </div>
+
+  <Meals :meals="meals" />
 </template>
 
 <script setup>
 import { computed, onMounted, ref } from "vue";
 import store from "../store";
 import { useRoute } from "vue-router";
-import YouTubeButton from "../components/YouTubeButton.vue";
-import MealItem from "../components/MealItem.vue";
+import Meals from "../components/Meals.vue";
 
 const route = useRoute();
 const keyword = ref("");
