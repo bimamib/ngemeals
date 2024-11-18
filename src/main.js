7523bbd79052dd949a327980102ccec9b86f174d
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-18 21:49:35 +0700

    Import

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import router from "./router";
 import store from "./store";
 import "./style.css";
 import App from "./App.vue";
-import * as filters from "./filter";
+import * as filters from "./filters";
 
 const app = createApp(App);
 app.use(router).use(store).mount("#app");
