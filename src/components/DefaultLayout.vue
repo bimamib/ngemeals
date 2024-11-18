139800d71e894c119ef05b13c5052f17cf27c48c
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-18 18:58:42 +0700

    Fix Default Layout

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="h-full bg-gray-100">
-    <navbar />
-    <main>
+  <Navbar />
+  <main>
+    <div class="max-w-[1200px] mx-auto">
       <router-view />
-    </main>
-  </div>
+    </div>
+  </main>
 </template>
 
 <script setup>
-import Navbar from "../components/Navbar.vue";
+import Navbar from "./Navbar.vue";
 </script>
