052713655669e9794ad5aeb6eefc8d41813f5200
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-18 18:59:16 +0700

    Fix Meals

--- src/components/Meals.vue
+++ src/components/Meals.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="grid grid-cols-1 gap-5 p-8 md:grid-cols-3">
+  <div
+    class="grid grid-cols-1 gap-5 px-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"
+  >
     <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
   </div>
-  <div v-if="!meals.length" class="flex justify-center text-slate-600">
+  <div v-if="!meals.length" class="flex justify-center p-8 text-slate-600">
     There are no meals
   </div>
 </template>
@@ -14,7 +16,6 @@ const { meals } = defineProps({
   meals: {
     required: true,
     type: Array,
-    default: () => [],
   },
 });
 </script>
