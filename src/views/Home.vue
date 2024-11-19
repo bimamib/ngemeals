78a565e04c2ca8e3ecb42e5f5b7376e5808431a5
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-19 22:55:41 +0700

    Fixing Bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+  <Loading :visible="loading" />
   <div class="flex flex-col p-8 pb-0 text-red-600">
     <h1 class="mb-4 text-4xl font-bold">Random Meals</h1>
   </div>
@@ -10,14 +11,17 @@ import { computed, onMounted, ref } from "vue";
 import store from "../store";
 import axiosClient from "../axiosClient";
 import Meals from "../components/Meals.vue";
+import Loading from "../components/Loading.vue";
 
 const meals = ref([]);
+const loading = ref(true);
 
 onMounted(async () => {
   for (let i = 0; i < 10; i++) {
-    axiosClient
-      .get(`random.php`)
-      .then(({ data }) => meals.value.push(data.meals[0]));
+    await axiosClient.get(`random.php`).then(({ data }) => {
+      meals.value.push(data.meals[0]);
+    });
   }
+  loading.value = false;
 });
 </script>
