56af43b4a8c4d7b4f6c338b9e5374cca2704d4f3
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-19 23:00:27 +0700

    Add: Feature Loading

---
+++ src/components/Loading.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    v-if="visible"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-50 backdrop-blur-md"
+  >
+    <div
+      class="w-16 h-16 border-t-4 border-b-4 border-white rounded-full animate-spin"
+    ></div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  visible: Boolean,
+});
+</script>
+
+<style scoped>
+/* Optional: Customize the spinning animation if needed */
+</style>
