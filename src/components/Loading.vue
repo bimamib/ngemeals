d09e829b81359931388a9ec2b6254d7825acd466
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-20 15:25:27 +0700

    Update Loading Bar

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -3,9 +3,15 @@
     v-if="visible"
     class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-50 backdrop-blur-md"
   >
-    <div
-      class="w-16 h-16 border-t-4 border-b-4 border-white rounded-full animate-spin"
-    ></div>
+    <div class="flex flex-col items-center justify-center w-full gap-4">
+      <div
+        class="flex items-center justify-center text-4xl text-blue-500 border-4 border-transparent rounded-full w-14 h-14 animate-spin border-t-blue-500"
+      >
+        <div
+          class="flex items-center justify-center w-10 h-10 text-2xl text-red-500 border-4 border-transparent rounded-full animate-spin border-t-red-500"
+        ></div>
+      </div>
+    </div>
   </div>
 </template>
 
